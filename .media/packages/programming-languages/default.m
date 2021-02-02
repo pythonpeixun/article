f4a9f4ee79e290e03137117cb6f6dc2d4285363a
brotherhuang <51mmj.com@gmail.com>
2021-02-02 14:39:15 +0800

    .media directory

--- .media/packages/programming-languages/default.m
+++
@@ -1,9 +0,0 @@
-/*****************************************************************************
- * -*- coding:utf-8 -*-
- * file name: {{title}}.m
- * description: TODO
- * create date: {{date}}
- * author: {{author}}
- * This file is created by Marboo<http://marboo.io> template file $MARBOO_HOME/.media/starts/default.m
- * 本文件由 Marboo<http://marboo.io> 模板文件 $MARBOO_HOME/.media/starts/default.m 创建
- ****************************************************************************/
--- .media/starts/default.m
+++
@@ -1,9 +0,0 @@
-/*****************************************************************************
- * -*- coding:utf-8 -*-
- * author: amoblin <amoblin@gmail.com>
- * file name: {{title}}.m
- * description: TODO
- * create date: {{date}}
- * This file is created from $MARBOO_HOME/.media/starts/default.m
- * 本文件由 $MARBOO_HOME/.media/starts/default.m 复制而来
- ****************************************************************************/
